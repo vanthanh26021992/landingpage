40a12517a29437afaae6ff52d58ea4ada56e2989
Thanh Tran <vanthanh260292@gmail.com>
2021-07-03 11:09:57 +0700

    create backend source code

---
+++ backend/src/test/java/com/landingpage/LandingpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landingpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandingpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
