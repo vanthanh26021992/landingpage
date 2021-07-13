a468466c8e6ea950728f40331e9eeaf6409bc86b
Hieu Do <hieudo062@gmail.com>
2021-07-13 20:41:05 +0700

    fist commit

--- backend/src/main/java/com/landingpage/LandingpageApplication.java
+++ backend/src/main/java/com/landingpage/LandingpageApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LandingpageApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LandingpageApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(LandingpageApplication.class, args);
+  }
 
 }
