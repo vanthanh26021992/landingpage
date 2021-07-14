7fa80d137355d58c54d009d4e27dc4d0117a8125
Hieu Do <hieudo062@gmail.com>
2021-07-14 21:07:43 +0700

    commit file from account manager project

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {MenubarModule} from 'primeng/menubar';
+import {MenuItem} from 'primeng/api';
+import axios from 'axios';
+import {ButtonModule} from 'primeng/button';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'frontend2';
+  items: MenuItem[] = [];
+
+
+  ngOnInit() {
+    this.items = [
+        {
+            label: 'File',
+            items: [{
+                    label: 'New', 
+                    icon: 'pi pi-fw pi-plus',
+                    items: [
+                        {label: 'Project'},
+                        {label: 'Other'},
+                    ]
+                },
+                {label: 'Open'},
+                {label: 'Quit'}
+            ]
+        },
+        {
+            label: 'Edit',
+            icon: 'pi pi-fw pi-pencil',
+            items: [
+                {label: 'Delete', icon: 'pi pi-fw pi-trash'},
+                {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
+            ]
+        }
+    ];
+}
+
+  add(){
+    console.log("add");
+  }
+
+  update(){
+    console.log("update");
+  }
+
+  delete(){
+    console.log("delete");
+  }
+
+  changePW(){
+    console.log("changePW");
+  }
+
+
+
+  createAccount() {
+    axios.get('localhost:8080/account/search').then(data => {
+      console.log(data)
+    }, error => {
+      console.log(error)
+    })
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {MenubarModule} from 'primeng/menubar';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MenubarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
