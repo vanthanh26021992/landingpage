42e2137b5d101d15b260db46922c35378a1b0923
Thanh Tran <vanthanh260292@gmail.com>
2021-07-15 20:15:08 +0700

    fix bug

--- backend/src/main/java/com/landingpage/controller/AccountController.java
+++ backend/src/main/java/com/landingpage/controller/AccountController.java
@@ -3,8 +3,8 @@ package com.landingpage.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +20,7 @@ public class AccountController {
   
   
   @PostMapping("create")
-  public List<Account> createAccount(Account account) {
+  public List<Account> createAccount(@RequestBody Account account) {
     return service.createAccount(account);
   }
   
