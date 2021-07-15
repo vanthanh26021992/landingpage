a5c4156774a93bad7456668af6b1f6890c7b051e
Thanh Tran <vanthanh260292@gmail.com>
2021-07-15 19:43:29 +0700

    fix bug cors

--- backend/src/main/java/com/landingpage/SimpleCORSFilter.java
+++ backend/src/main/java/com/landingpage/SimpleCORSFilter.java
@@ -1,50 +1,22 @@
 package com.landingpage;
 
-import java.io.IOException;
-
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpMethod;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Component
-public class SimpleCORSFilter implements Filter {
+@Configuration
+public class SimpleCORSFilter implements WebMvcConfigurer {
 
-private final Logger log = LoggerFactory.getLogger(SimpleCORSFilter.class);
+  private final Logger log = LoggerFactory.getLogger(SimpleCORSFilter.class);
 
-public SimpleCORSFilter() {
+  public SimpleCORSFilter() {
     log.info("SimpleCORSFilter init");
-}
-
-@Override
-public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-  final HttpServletResponse response = (HttpServletResponse) res;
-  response.setHeader("Access-Control-Allow-Origin", "*");
-  response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-  response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, enctype");
-  response.setHeader("Access-Control-Max-Age", "4200");
-  if (HttpMethod.OPTIONS.name().equalsIgnoreCase(((HttpServletRequest) req).getMethod())) {
-      response.setStatus(HttpServletResponse.SC_OK);
-  } else {
-      chain.doFilter(req, res);
   }
-}
 
-@Override
-public void init(FilterConfig filterConfig) {
-}
-
-@Override
-public void destroy() {
-}
-
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**").allowedMethods("*");
+  }
 }
